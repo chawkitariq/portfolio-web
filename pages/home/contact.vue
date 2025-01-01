32892b7352995feb7bae51a35df9aad6116993fb
chawkitariq <chawkitariq@icloud.com>
2025-01-01 17:18:59 +0100

    refactor: remove console.log

--- pages/home/contact.vue
+++ pages/home/contact.vue
@@ -110,10 +110,6 @@ const emailjsPublicKey = runtimeConfig.public.emailjsPublicKey
 const emailjsServiceId = runtimeConfig.public.emailjsServiceId
 const emailjsTemplateId = runtimeConfig.public.emailjsTemplateId
 
-console.log(emailjsPublicKey)
-console.log(emailjsServiceId)
-console.log(emailjsTemplateId)
-
 emailjs.init({
     publicKey: emailjsPublicKey,
     blockHeadless: true,
@@ -137,7 +133,6 @@ const handleSubmit = form.handleSubmit(async (payload) => {
             description: 'Votre message a été envoyé avec succès. Nous vous répondrons sous peu.',
         });
     } catch (error) {
-        console.log(error)
         toast({
             variant: 'destructive',
             title: "Erreur d'envoi",
